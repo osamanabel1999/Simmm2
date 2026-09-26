49d17c23d4e46136d39914361c7401a3d61a3af0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-09-26 08:45:39 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/i_ppage_xplane/i_ppage_xplane_widget.dart
+++ lib/pages/i_ppage_xplane/i_ppage_xplane_widget.dart
@@ -472,7 +472,7 @@ class _IPpageXplaneWidgetState extends State<IPpageXplaneWidget> {
                           safeSetState(() {});
 
                           context.pushNamed(
-                            EFBappPagesXPlaneWidget.routeName,
+                            EFBxplaneWidget.routeName,
                             extra: <String, dynamic>{
                               '__transition_info__': TransitionInfo(
                                 hasTransition: true,
